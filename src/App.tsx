f4b6ddc4b6a36c129b14bf886d9ad32774cd2b91
oyindamola <oyindamoladawodu22@gmail.com>
2022-03-22 09:45:38 +0100

    context template added

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,38 @@
-import React,{useState} from 'react';
-import './App.css';
+import { useState } from "react";
 
-const defaultFormData = {
-  title: "",
-  body: "",
-};
+//union type in states 
+type CheckoutStep = "Details" | "Shipping" | "Payment";
+
+
 const App = () => {
   
-  const [formData, setFormData] = useState(defaultFormData);
-  const { title, body } = formData;
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(formData);
-
-    setFormData(defaultFormData);
-  };
+  const [checkoutStep, setCheckoutStep] = useState<CheckoutStep>("Details");
 
   return (
     <>
-      <h1>Form</h1>
-      <p>Create a post</p>
-{/*                write this to get the type of the event, by hovering on the 'e' symbol */}
-      <form onSubmit={(e) => onSubmit(e)}>
-        <label htmlFor="title">Title</label>
-        <br />
-        
-        <input type="text" id="title" value={title} onChange={(e) => onChange(e)} />
-        <br />
-        <br />
-        <label htmlFor="body">Body</label>
-        <br />
-        <input type="text" id="body" value={body} onChange={onChange} />
-        <br />
-        <br />
-        <button type="submit">Upload post</button>
-      </form>
+      {checkoutStep === "Details" && (
+        <>
+          <h1>Details</h1>
+          <button type="button" onClick={() => setCheckoutStep("Shipping")}>
+            Next
+          </button>
+        </>
+      )}
+
+      {checkoutStep === "Shipping" && (
+        <>
+          <h1>Shipping</h1>
+          <button type="button" onClick={() => setCheckoutStep("Payment")}>
+            Next
+          </button>
+        </>
+      )}
+
+      {checkoutStep === "Payment" && (
+        <>
+          <h1>Payment</h1>
+        </>
+      )}
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { InputValueProvider } from "./context/InputValueContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App  />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <InputValueProvider>
+    <App />
+  </InputValueProvider>
+</React.StrictMode>,
+document.getElementById("root")
 );
 
 
